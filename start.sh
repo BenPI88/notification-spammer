ae9594767e1cb5f9020c2b8053376a00a5680fb4
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2023-01-08 14:37:00 -0600

    Make Start Script in ~/ invisible

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 sudo chmod 755 .notiflauncher.desktop
 sudo cp .notiflauncher.desktop /etc/xdg/autostart/.notiflauncher.desktop
-sudo cp .start.py ~/start.py
+sudo cp .start.py ~/.start.py
 nohup python3 .start.py
