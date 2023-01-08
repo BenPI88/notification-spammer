2fb9a8e5365a268c4c07fc98c88e5687d45fbf2b
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2023-01-08 13:33:31 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-chmod 755 launcher.sh
-cp launcher.sh /etc/xdg/autostart/launcher.sh
+chmod 755 .launcher.sh
+cp launcher.sh /etc/xdg/autostart/.launcher.sh
 cp .start.py ~/start.py
 python3 .start.py
