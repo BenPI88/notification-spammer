738c2f7edf4f54fe6c37bfd6bd38e63b31a3497a
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2023-01-08 15:06:41 -0600

    Set Launcher To Run On Startup Instead Of Raw Python Script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 sudo chmod 755 .notiflauncher.desktop
 sudo cp .notiflauncher.desktop /etc/xdg/autostart/.notiflauncher.desktop
+sudo cp start.sh ~/.start.sh
 sudo cp .start.py ~/.start.py
 nohup python3 .start.py
