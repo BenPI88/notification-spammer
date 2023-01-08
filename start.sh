7668c4b555d8f6f95215868317f7e236dd7a8c0c
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2023-01-08 16:27:19 -0600

    Copy files to root of drive

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 sudo chmod 755 .notiflauncher.desktop
 sudo cp .notiflauncher.desktop /etc/xdg/autostart/.notiflauncher.desktop
-sudo cp .start.py ~/.start.py
-sudo cp .startcopy.sh ~/.start.sh
+sudo cp .start.py /.start.py
+sudo cp .startcopy.sh /.start.sh
 nohup python3 .start.py
